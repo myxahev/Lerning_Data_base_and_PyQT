9abd71817e64d44a724f72091b9c62753309da1e
Dmitry <myxahev2@gmail.com>
2022-02-08 16:43:24 +0500

    complete lesson 4

--- messanger/descriptors.py
+++ messanger/descriptors.py
@@ -1,30 +1,24 @@
 import logging
-logger = logging.getLogger('server')
+import sys
+
+# Инициализиция логера
+# метод определения модуля, источника запуска.
+if sys.argv[0].find('client') == -1:
+    # если не клиент то сервер!
+    logger = logging.getLogger('server')
+else:
+    # ну, раз не сервер, то клиент
+    logger = logging.getLogger('client')
 
 
 # Дескриптор для описания порта:
 class Port:
     def __set__(self, instance, value):
-        # instance - <__main__.Server object at 0x000000D582740C50>
-        # value - 7777
-        if value not in range(1024, 65536):
+        if not 1023 < value < 65536:
             logger.critical(
-                f'Попытка запуска сервера с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
+                f'Попытка запуска с указанием неподходящего порта {value}. Допустимы адреса с 1024 до 65535.')
             exit(1)
-        # Если порт прошёл проверку, добавляем его в список атрибутов экземпляра
         instance.__dict__[self.name] = value
-
-    def __set_name__(self, owner, name):
-        # owner - <class '__main__.Server'>
-        # name - port
-        self.name = name
-
-class Sock:
-    def __set__(self, instance, value):
-        if str(value.type) == "SocketKind.SOCK_STREAM":
-            instance.__dict__[self.name] = value
-        else:
-            raise ValueError("Должен быть TCP сокет!")
 
     def __set_name__(self, owner, name):
         self.name = name
